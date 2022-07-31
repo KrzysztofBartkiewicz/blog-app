d7022e495394209b608e51dc8ff018248e6649bd
KrzysztofBartkiewicz <bartkiewicz.krzysztof84@gmail.com>
2022-07-31 17:53:07 +0200

    Add projects route

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,19 +4,22 @@ type LayoutProps = {
   children: React.ReactNode
 }
 
+const navLinks = [
+  { path: '/', label: 'Blog', id: 1 },
+  { path: '/about', label: 'About', id: 2 },
+  { path: '/projects', label: 'Projects', id: 3 }
+]
+
 const Layout = ({ children }: LayoutProps) => (
   <div className="p-10 font-mono">
     <nav className="bg-blue-400 p-5 flex justify-center">
-      <Link href="/">
-        <a className="shaddow md:w-40 bg-blue-50 p-2 m-2 text-center hover:bg-gray-50 cursor-pointer">
-          Blog
-        </a>
-      </Link>
-      <Link href="/about">
-        <a className="shaddow md:w-40 bg-blue-50 p-2 m-2 text-center hover:bg-gray-50 cursor-pointer">
-          About
-        </a>
-      </Link>
+      {navLinks.map(({ path, label, id }) => (
+        <Link href={path} key={id}>
+          <a className="shaddow md:w-40 bg-blue-50 p-2 m-2 text-center hover:bg-gray-50 cursor-pointer">
+            {label}
+          </a>
+        </Link>
+      ))}
     </nav>
 
     <main className="bg-gray-100 p-5">{children}</main>
---
+++ src/pages/projects.tsx
@@ -0,0 +1,7 @@
+import Layout from '@src/components/Layout'
+
+const Projects = () => {
+  return <Layout>All my projects</Layout>
+}
+
+export default Projects
